ffd68f7a0ac7654a2f256e78f720bb3796ef4b49
Matteo-Caridi <caridimatteo0@outlook.it>
2021-04-06 14:50:34 +0200

    creata classe User

---
+++ class/User.php
@@ -0,0 +1,18 @@
+<?php
+
+class User{
+    private $userId;
+    private $firstName;
+    private $lastName;
+    private $birthday;
+    private $email;
+
+    public function __construct(int $userId, string $firstName, string $lastName, DateTime $birthday, string $email){
+        $this->userId = $userId;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->birthday = $birthday;
+        $this->email = $email;
+    }
+
+}
